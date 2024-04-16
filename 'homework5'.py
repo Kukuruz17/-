6615f5bf799fe64fa067e3842a96e6b5e62978a5
Kukuruz17 <166056380+Kukuruz17@users.noreply.github.com>
2024-04-16 14:37:40 +0300

    Add files via upload

---
+++ 'homework5'.py
@@ -0,0 +1,13 @@
+my_list = ['яблоко', 'слон', 'перец','пеплац', "машина времени", "Prodigy"]
+print(my_list)
+print(my_list[0::5])
+print(my_list[2:4])
+my_list[3] = "любовь"
+print(my_list)
+my_dict = {'Яблоко': 'apple', 'Мозг': 'brain', 'Подруга': 'girlfriend','Муравей': 'Prodigy'}
+print(my_dict)
+print(my_dict['Мозг'])
+my_dict['Подруга'] = 12345
+print(my_dict)
+my_dict.update({'Разгерметизация': 'depressurization'})
+print(my_dict)
