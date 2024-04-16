62bd4c5a74f416e622e13622971b1aae68cfcd85
Kukuruz17 <166056380+Kukuruz17@users.noreply.github.com>
2024-04-16 13:15:18 +0300

    Delete 'homework4'.py

--- 'homework4'.py
+++
@@ -1,6 +0,0 @@
-immutable_var = 1, 2, 3, 'story'
-print(immutable_var)
-mutable_list = ["кукуруза", 'ferrary','07.09.1986',45, 29,'32']
-print(mutable_list)
-mutable_list[1] = "не для наших дорог"
-print(mutable_list)
